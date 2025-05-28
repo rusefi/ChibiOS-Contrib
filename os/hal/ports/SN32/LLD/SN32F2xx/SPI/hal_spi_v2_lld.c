8c4e1e24f78990f6ac47da2fe21f7cccabda3ab1
sergey radionov <rsatom@gmail.com>
2025-05-28 11:47:50 +0700

    sn32: spi: cosmetic refactoring

--- os/hal/ports/SN32/LLD/SN32F2xx/SPI/hal_spi_v2_lld.c
+++ os/hal/ports/SN32/LLD/SN32F2xx/SPI/hal_spi_v2_lld.c
@@ -58,12 +58,13 @@ SPIDriver SPID1;
 
 static void spi_lld_configure(SPIDriver *spip) {
   spip->spi->CTRL0 = spip->config->ctrl0;
-  spip->spi->CTRL1 = (uint32_t)spip->config->ctrl1;
-  spip->spi->CLKDIV = (uint32_t)spip->config->clkdiv;
-
-  spip->spi->CTRL0 |= (spip->config->slave << 3);
-  spip->spi->CTRL0_b.SDODIS = false;
   spip->spi->CTRL0_b.SELDIS = SPI_SELECT_MODE != SPI_SELECT_MODE_LLD;
+  spip->spi->CTRL0_b.MS = spip->config->slave;
+  spip->spi->CTRL0_b.SDODIS = false;
+
+  spip->spi->CTRL1 = (uint32_t)spip->config->ctrl1;
+
+  spip->spi->CLKDIV = (uint32_t)spip->config->clkdiv;
 
   uint32_t sn32_spi_clock = (SN32_HCLK / ((2 * spip->config->clkdiv) + 2));
   if (sn32_spi_clock > 6000000) {
